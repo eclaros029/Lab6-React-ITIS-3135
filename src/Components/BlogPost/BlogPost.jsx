e34c4a57f4f5f23c7346abd55fb9e582c21bb883
AlanUL <75558402+AlanUL@users.noreply.github.com>
2025-10-25 15:01:12 -0400

    Update BlogPost.jsx

--- src/Components/BlogPost/BlogPost.jsx
+++ src/Components/BlogPost/BlogPost.jsx
@@ -2,26 +2,22 @@ import React from "react";
 import CommentList from "../CommentList/CommentList";
 import CommentForm from "../CommentForm/CommentForm";
 
-function BlogPost({post})
-{
+function BlogPost({ post }) {
     const { title, content, author, date } = post;
 
     return (
-        <article className="blog-post-container">
+        <article className="post-card">
             <header>
                 <h2>{title}</h2>
                 <p>{content}</p>
             </header>
-            <section className="post-meta"
-                //style={{add syling here}}
-            >
-                <p><strong>Author:</strong>{author}</p>
-                <p><strong>Date:</strong>{date}</p>
+
+            <section className="post-meta">
+                <p><strong>Author:</strong> {author}</p>
+                <p><strong>Date:</strong> {date}</p>
             </section>
 
-            <section className="comments-section"
-                //style={{add syling here}}
-            >
+            <section className="comments">
                 <h3>Comments</h3>
                 <CommentForm />
                 <CommentList />
